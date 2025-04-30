8515c9b48dfb4b443bc89aa8b69e17cfe965f7c9
AThevon <adrienthevon@gmail.com>
2025-04-30 20:42:35 +0200

    deploy: fix bug routing

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -3,30 +3,32 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        $this->routes(function () {
-            if (app()->environment('local')) {
-                Route::middleware('web')
-                    ->group(base_path('routes/web.php'));
+  public function boot(): void
+  {
+    if (app()->environment('local')) {
+      // Routes web (Filament/admin)
+      Route::middleware('web')
+        ->group(base_path('routes/web.php'));
 
-                Route::middleware('api')
-                    ->prefix('api')
-                    ->group(base_path('routes/api.php'));
-            } else {
-                Route::middleware('web')
-                    ->domain('admin.undertheflow.com')
-                    ->group(base_path('routes/web.php'));
+      // Routes API
+      Route::middleware('api')
+        ->prefix('api')
+        ->group(base_path('routes/api.php'));
+    } else {
+      // Routes web (admin.undertheflow.com)
+      Route::middleware('web')
+        ->domain('admin.undertheflow.com')
+        ->group(base_path('routes/web.php'));
 
-                Route::middleware('api')
-                    ->domain('api.undertheflow.com')
-                    ->prefix('api')
-                    ->group(base_path('routes/api.php'));
-            }
-        });
+      // Routes API (api.undertheflow.com)
+      Route::middleware('api')
+        ->domain('api.undertheflow.com')
+        ->prefix('api')
+        ->group(base_path('routes/api.php'));
     }
+  }
 }
